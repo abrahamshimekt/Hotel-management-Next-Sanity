1118e2c84998158ee01459fc90a5d8aba00a0abb
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-05-08 20:27:05 +0300

    define schema types

---
+++ schemaTypes/index.ts
@@ -0,0 +1,9 @@
+import { access } from "fs";
+import user from "./user";
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
+import verficationToken from "./verficatoinToken";
+
+export const schemaTypes = [user,account,booking,hotelRoom,review,verficationToken]
