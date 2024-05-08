15998eb6c9364d7276bd44457d8768a2da62fd7d
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-05-08 20:27:30 +0300

    configure

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images :{
+        domains:['lh3.googleusercontent.com','images.unsplash.com',"unsplash.com","media.istockphoto.com","images.trvl-media.com","www.ethiopianskylighthotel.com","bookings.hotelrez.co.uk"]
+    }
+};
+
+export default nextConfig;
